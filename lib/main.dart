1ec3cfef0404349c96140fc3b9c05201db7ee433
Jonathan Medina <0428365@gmail.com>
2026-03-28 13:33:21 -0400

    Initial commit for Blue Light Mapping app

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CampusSafetyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CampusSafetyApp extends StatelessWidget {
+  const CampusSafetyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'UM Campus Safety Route',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const CampusMapScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CampusMapScreen extends StatefulWidget {
+  const CampusMapScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CampusMapScreen> createState() => _CampusMapScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CampusMapScreenState extends State<CampusMapScreen> {
+  // Center of University of Miami Campus
+  final LatLng umCenter = const LatLng(25.7173, -80.2781);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Mock User Location (Slightly offset from center)
+  final LatLng userLocation = const LatLng(25.7185, -80.2790);
+
+  List<LatLng> blueLightPhones = [];
+  List<LatLng> jaggedPath = [];
+  final Random _random = Random();
+
+  @override
+  void initState() {
+    super.initState();
+    _generateRandomPhones();
+    _generateJaggedPath();
+  }
+
+  // Generates 5 random locations around the campus center
+  void _generateRandomPhones() {
+    for (int i = 0; i < 5; i++) {
+      // Create a small random offset for latitude and longitude
+      double latOffset = (_random.nextDouble() - 0.5) * 0.01;
+      double lngOffset = (_random.nextDouble() - 0.5) * 0.01;
+      blueLightPhones.add(
+          LatLng(umCenter.latitude + latOffset, umCenter.longitude + lngOffset)
+      );
+    }
+  }
+
+  // Simulates a non-straight walking path to the first random phone
+  void _generateJaggedPath() {
+    LatLng targetPhone = blueLightPhones.first;
+
+    // Create random intermediate waypoints to make the line jagged
+    LatLng waypoint1 = LatLng(
+      (userLocation.latitude + targetPhone.latitude) / 2 + 0.001,
+      (userLocation.longitude + targetPhone.longitude) / 2 - 0.002,
+    );
+    LatLng waypoint2 = LatLng(
+      (userLocation.latitude + targetPhone.latitude) / 2 - 0.0015,
+      (userLocation.longitude + targetPhone.longitude) / 2 + 0.001,
+    );
+
+    // Build the path sequence
+    jaggedPath = [userLocation, waypoint1, waypoint2, targetPhone];
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Safety Route Prototype'),
+        backgroundColor: Colors.blue[900],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: umCenter,
+          initialZoom: 15.5,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        children: [
+          // 1. The OpenStreetMap Tile Layer
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.campussafety',
+          ),
+
+          // 2. The Jagged Path Layer
+          PolylineLayer(
+            polylines: [
+              Polyline(
+                points: jaggedPath,
+                color: Colors.green, // Green to signify a "Safe Route"
+                strokeWidth: 4.0,
+              ),
+            ],
+          ),
+
+          // 3. The Markers Layer (User + 5 Phones)
+          MarkerLayer(
+            markers: [
+              // User Marker (Red Pin)
+              Marker(
+                point: userLocation,
+                width: 40,
+                height: 40,
+                child: const Icon(Icons.person_pin_circle, color: Colors.red, size: 40),
+              ),
+              // Blue Light Phone Markers (Generated List)
+              ...blueLightPhones.map(
+                    (phoneLoc) => Marker(
+                  point: phoneLoc,
+                  width: 40,
+                  height: 40,
+                  child: const Icon(Icons.emergency_share, color: Colors.blue, size: 30),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
